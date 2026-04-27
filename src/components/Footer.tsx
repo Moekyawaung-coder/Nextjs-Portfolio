a478d2c65f569532eb6b4e7c1f21dca9b6e38ae7
Moekyawunng <moekyawaung@diplomats.com>
2026-04-27 23:41:36 +0630

    Add Footer component with copyright notice

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className="py-6 text-center bg-gray-800 text-gray-400">
+      © 2026 Moe Kyaw Aung. All rights reserved.
+    </footer>
+  )
+}
